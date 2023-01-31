15eab8007591b694d147af9a0af0b51e979e579a
unknown <vishwab1>
2023-01-31 16:44:29 +0530

    C# commit

---
+++ Codes/SqlExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+using MySql.Data.MySqlClient;
+class SqlExample
+{
+
+    static void Main(string[] args)
+    {
+
+        string sql = "server=localhost;userid=root;password=sql123;database=db_example";
+
+        var con = new MySqlConnection(sql);
+        con.Open();
+        //con.ConnectionString= sql;
+
+        //var stm = "SELECT * FROM books";
+        //MySqlCommand mySqlCommand = new MySqlCommand(stm, con);
+        //var stm= "INSERT INTO books(bookid,author,bookname,price) VALUES(@bookid,@author,@bookname,@price)";
+        //var stm = " delete from books where bookid=@bookid";
+        var stm = " update  books set author=@author,bookname=@bookname,price=@price where bookid=@bookid";
+
+        MySqlCommand mySqlCommand = new MySqlCommand(stm, con);
+        mySqlCommand.Parameters.AddWithValue("@bookid", 111);
+        mySqlCommand.Parameters.AddWithValue("@author", "vishwa");
+        mySqlCommand.Parameters.AddWithValue("@bookname", "java life");
+        mySqlCommand.Parameters.AddWithValue("@price", 3666);
+        mySqlCommand.Prepare();
+
+        mySqlCommand.ExecuteNonQuery();
+
+        //var version = mySqlCommand.ExecuteScalar().ToString();
+       /* MySqlDataReader readeer = mySqlCommand.ExecuteReader();
+
+        while (readeer.Read())
+        {
+         Console.WriteLine("{0} {1}", readeer.GetInt32(0), readeer.GetString(1));
+        }*/
+
+
+    }
+
+
+}
+    
---
+++ Codes/ThreadExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+class ThreadExample
+{
+
+    static void Main(string[] args)
+    {
+
+
+        Thread th = Thread.CurrentThread;
+        th.Name = "MainThread";
+
+        Console.WriteLine("This is {0}", th.Name);
+
+        ThreadStart childref = new ThreadStart(CallToChildThread);
+        Thread childThread = new Thread(childref);
+        childThread.Start();
+        //Thread.Sleep(2000);
+
+        //now abort the child
+        Console.WriteLine("In Main: Aborting the Child thread");
+
+        childThread.Interrupt();
+
+    }
+
+    private static void CallToChildThread()
+    {
+
+        try
+        {
+            Console.WriteLine("Child thread starts");
+
+            // do some work, like counting to 10
+            for (int counter = 0; counter <= 10; counter++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine(counter);
+            }
+
+            Console.WriteLine("Child Thread Completed");
+        }
+        catch (ThreadAbortException e)
+        {
+            Console.WriteLine("Thread Abort Exception");
+        }
+        finally
+        {
+            Console.WriteLine("Couldn't catch the Thread Exception");
+        }
+    }
+}
+    
